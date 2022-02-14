b1ed62af9696c8911be17813d53ab915fd08f468
Jo Thomas <jothomas1996@gmail.com>
2022-02-14 21:06:37 +0530

    initialized

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php include("common/head.php"); ?>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<p>Test</p>
+				</div>
+			</div>
+		</div>
+
+		<?php include("common/foot.php"); ?>
+	</body>
+</html>
