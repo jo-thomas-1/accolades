e31733046244150cf8083efa522828cf9f4f46fd
Jo Thomas <jothomas1996@gmail.com>
2022-02-15 22:48:35 +0530

    removed reduntant read function from read.php

--- database_handle/read.php
+++ database_handle/read.php
@@ -1,32 +1,4 @@
 <?php
-    // common database read function
-    function read_table($table, $columns)
-    {
-        $connection = mysqli_connect("localhost","root","","accolades");
-
-        // check connection status
-        if (mysqli_connect_errno())
-        {
-            echo "Failed to connect to Database: " . mysqli_connect_error();
-            return(false);
-            exit();
-        }
-
-        $sql = "SELECT " . $columns . " FROM " . $table;
-        $result = mysqli_query($connection, $sql);
-
-        // fetch all data
-        $data = json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
-
-        // free result set
-        mysqli_free_result($result);
-
-        // close connection
-        mysqli_close($connection);
-
-        return($data);
-    }
-
     // execute SQL command and read data
     function execute_and_read($sql)
     {
@@ -39,7 +11,7 @@
             return(false);
             exit();
         }
-        
+
         $result = mysqli_query($connection, $sql);
 
         // fetch all data
